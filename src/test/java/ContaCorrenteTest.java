31129f0c3fedaea714815aa04fe459290a855a1b
Robson Agapito <rnai@gft.com>
2023-11-14 14:12:50 -0300

    teste quebrado.

--- src/test/java/ContaCorrenteTest.java
+++ src/test/java/ContaCorrenteTest.java
@@ -35,8 +35,8 @@ public class ContaCorrenteTest {
         //arrange
         ContaCorrente contaCorrente = new ContaCorrente();
         double valor = 10;
-        double esperado = 10;
-        double resultado = 1;
+        double esperado = 1;
+        double resultado = 0;
 
         //act
         resultado = contaCorrente.depositar(valor);
